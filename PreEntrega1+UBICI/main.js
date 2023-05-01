feaddec3b6f23241f7bd1283d7e23b571f23d135
Paula Ubici <paulaubici@gmail.com>
2023-05-01 14:07:33 -0300

    Agrego condicional + ciclo + function

--- PreEntrega1+UBICI/main.js
+++ PreEntrega1+UBICI/main.js
@@ -25,8 +25,14 @@ if(meta >= 0 && meta<=12){
 let libros = Number(prompt("¿Cuántos libros llevas leídos?"));
 let resultado = meta - libros; 
 let mensaje4 = `${nombre}, tienes que leer ${resultado} libros para alcanzar tu meta.`
-alert(mensaje4)
+alert(mensaje4);
 
+
+for(let meses = 6; meses <= 12; meses++){
+     let resultado2 = resultado / meses;
+
+alert(`Si te faltan leer ${resultado} libros para alcanzar tu meta y tienes ${meses} meses, deberás leer ${resultado2} libros por mes`);
+}
 
 
 
