c9acec137a46ef100eefd9d0e99cf247887ec52d
Paula Ubici <paulaubici@gmail.com>
2023-05-22 12:39:44 -0300

    Agrego métodos de busqueda

--- PreEntrega2+UBICI/main.js
+++ PreEntrega2+UBICI/main.js
@@ -1,6 +1,5 @@
 let nombre = prompt("Ingrese su nombre para comenzar:");
 
-
 function saludar() {
     let mensaje = `¡Bienvenid@, ${nombre}!`;
     alert(mensaje);
@@ -13,37 +12,95 @@ class Libro{
       this.titulo = titulo;
       this.autor = autor;
       this.precio = precio
-      this.lista = lista; 
+      
       };
   
   }
 
   const Libros = [
 
-    {id: 1, titulo: "Rayuela", autor: "Julio Cortazar", precio: 3999.00 , lista: false},
-    {id: 2, titulo: "El Túnel", autor: "Ernesto Sábato", precio: 2999.00 , lista: false},
-    {id: 3, titulo: "Ficciones", autor: "Jorge Luis Borges", precio: 4999.00 , lista: false},
-    {id: 4, titulo: "El Segundo Sexo", autor: "Simone de Beauvoir", precio: 5999.00 , lista: false},
-    {id: 5, titulo: "Jurassic Park", autor: "Michael Crichton", precio: 6999.00 , lista: false},
-    {id: 6, titulo: "Los Siete Maridos de Evelyn Hugo", autor: "Taylor Jenkins Reid", precio: 7999.00 , lista: false},
-    {id: 7, titulo: "Todos Quieren a Daisy Jones", autor: "Taylor Jenkins Reid", precio: 7500.00 , lista: false},
+    {
+     id: 1, 
+     titulo: "Rayuela", 
+     autor: "Julio Cortazar", 
+     precio: 3999.00 ,
+    },
+    {
+      id: 2, 
+      titulo: "El Túnel", 
+      autor: "Ernesto Sábato", 
+      precio: 2999.00,
+    },
+    {
+    
+    id: 3, 
+    titulo: "Ficciones", 
+    autor: "Jorge Luis Borges", 
+    precio: 4999.00 , 
+    },
+    {
+      id: 4, 
+      titulo: "El Segundo Sexo", 
+      autor: "Simone de Beauvoir", 
+      precio: 5999.00,
+    },
+    {
+      id: 5, 
+      titulo: "Jurassic Park", 
+      autor: "Michael Crichton", 
+      precio: 6999.00 ,
+    },
+    {
+      id: 6, 
+      titulo: "Los Siete Maridos de Evelyn Hugo", 
+      autor: "Taylor Jenkins Reid", 
+      precio: 7999.00 , 
+    },
+    {
+      id: 7, 
+      titulo: "Todos Quieren a Daisy Jones", 
+      autor: "Taylor Jenkins Reid", 
+      precio: 7500.00 ,
+    },
     
     ]
 
-let busqueda = prompt("Ingrese título del libro que estás buscando.");
+let busqueda = prompt("Ingresa título del libro que estás buscando.");
 
-const encontrado = Libros.filter((item) => item.titulo.includes(busqueda));
+const encontrado = Libros.find((item) => item.titulo.includes(busqueda));
+console.log(encontrado)
 
-encontrado.forEach((item) => {
        let mensaje = `
        ¡Libro encontrado! :)
-        ID: ${item.id}
-        Título: ${item.titulo}
-        Autor: ${item.autor}
-        Precio: $${item.precio}
+        ID: ${encontrado.id}
+        Título: ${encontrado.titulo}
+        Autor: ${encontrado.autor}
+        Precio: $${encontrado.precio}
 
        `;
 
        alert(mensaje);
-        });
+      
 
+let suscripcion = prompt("¿Te gustaría asociarte a nuestro club? Accederás a un 20% de descuento en tus compras. S/N").toUpperCase();
+
+function descuento(encontrado){
+  if(suscripcion === "S"){
+    let PrecioConDesc = encontrado.precio - encontrado.precio * 0.20 
+
+
+       return encontrado = {
+       id: encontrado.id, 
+       titulo: encontrado.titulo,
+       autor: encontrado.autor,
+       precio: PrecioConDesc
+       };
+       
+     }
+  
+     
+   }
+
+   console.log(descuento(encontrado))
+
+   
