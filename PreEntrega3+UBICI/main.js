565b400adaa0d06f67dba140bf7b793fe8a5248f
Paula Ubici <paulaubici@gmail.com>
2023-06-02 22:07:03 -0300

    Correcciones para Pre-Entrega 3

--- PreEntrega3+UBICI/main.js
+++ PreEntrega3+UBICI/main.js
@@ -1,91 +1,92 @@
-// let libros = [
+  // let libros = [
 
-//   {
-//    id: 1, 
-//    titulo: "Rayuela", 
-//    autor: "Julio Cortazar", 
-//    precio: 3999.00 ,
-//   },
-//   {
-//     id: 2, 
-//     titulo: "El Túnel", 
-//     autor: "Ernesto Sábato", 
-//     precio: 2999.00,
-//   },
-//   {
-  
-//   id: 3, 
-//   titulo: "Ficciones", 
-//   autor: "Jorge Luis Borges", 
-//   precio: 4999.00 , 
-//   },
-//   {
-//     id: 4, 
-//     titulo: "El Segundo Sexo", 
-//     autor: "Simone de Beauvoir", 
-//     precio: 5999.00,
-//   },
-//   {
-//     id: 5, 
-//     titulo: "Jurassic Park", 
-//     autor: "Michael Crichton", 
-//     precio: 6999.00 ,
-//   },
-//   {
-//     id: 6, 
-//     titulo: "Los Siete Maridos de Evelyn Hugo", 
-//     autor: "Taylor Jenkins Reid", 
-//     precio: 7999.00 , 
-//   },
-//   {
-//     id: 7, 
-//     titulo: "Todos Quieren a Daisy Jones", 
-//     autor: "Taylor Jenkins Reid", 
-//     precio: 7500.00 ,
-//   },
-  
-//   ]
+  //   {
+  //    id: 1, 
+  //    titulo: "Rayuela", 
+  //    autor: "Julio Cortazar", 
+  //    precio: 3999.00 ,
+  //   },
+  //   {
+  //     id: 2, 
+  //     titulo: "El Túnel", 
+  //     autor: "Ernesto Sábato", 
+  //     precio: 2999.00,
+  //   },
+  //   {
+    
+  //   id: 3, 
+  //   titulo: "Ficciones", 
+  //   autor: "Jorge Luis Borges", 
+  //   precio: 4999.00 , 
+  //   },
+  //   {
+  //     id: 4, 
+  //     titulo: "El Segundo Sexo", 
+  //     autor: "Simone de Beauvoir", 
+  //     precio: 5999.00,
+  //   },
+  //   {
+  //     id: 5, 
+  //     titulo: "Jurassic Park", 
+  //     autor: "Michael Crichton", 
+  //     precio: 6999.00 ,
+  //   },
+  //   {
+  //     id: 6, 
+  //     titulo: "Los Siete Maridos de Evelyn Hugo", 
+  //     autor: "Taylor Jenkins Reid", 
+  //     precio: 7999.00 , 
+  //   },
+  //   {
+  //     id: 7, 
+  //     titulo: "Todos Quieren a Daisy Jones", 
+  //     autor: "Taylor Jenkins Reid", 
+  //     precio: 7500.00 ,
+  //   },
+    
+  //   ]
 
-//   localStorage.setItem("libros", JSON.stringify(libros)); 
+  //   localStorage.setItem("libros", JSON.stringify(libros)); 
 
 let formulario = document.getElementById("formulario");
 let agregados = document.getElementById("agregados");
+let librosContenedor = document.getElementById("librosContenedor");
+let libros = JSON.parse(localStorage.getItem("libros")) || [];
 
-  const ingresar = (titulo, autor) => {
-    let librosStorage = localStorage.getItem("libros"); 
-    let libros = JSON.parse(librosStorage);
-    
-    let nuevoLibro = {
-      ID: libros.length + 1,
-      Título: titulo, 
-      Autor: autor,
-    }; 
 
-    libros.push(nuevoLibro);
-    localStorage.setItem("libros", JSON.stringify(libros));
-  };
 
-  formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    let inputs = e.target.children;
-  
-    ingresar(inputs[0].value, inputs[1].value);
-  });
-  
-  agregados.addEventListener("click", () => {
-    let librosStorage = localStorage.getItem("libros");
-    let libros = JSON.parse(librosStorage);
-  
-    libros.forEach((libro) => {
-      let div = document.createElement("div");
-      div.innerHTML = `
+
+const ingresar = (titulo, autor) => {
+	let nuevoLibro = {
+		id: libros.length + 1,
+		titulo: titulo,
+		autor: autor,
+	};
+
+	libros.push(nuevoLibro);
+	localStorage.setItem("libros", JSON.stringify(libros));
+};
+
+formulario.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	let inputs = e.target.children;
+
+	ingresar(inputs[0].value, inputs[1].value);
+});
+
+agregados.addEventListener("click", () => {
+	librosContenedor.innerHTML = "";
+	libros.forEach((libro) => {
+		let div = document.createElement("div");
+		console.log(libro);
+		div.innerHTML = `
           <h2>Id: ${libro.id}</h2>
-          <p>Nombre: ${libro.titulo}</p>
-          <b>${libro.autor}</b>
+          <b>Nombre: ${libro.titulo}</b>
+          <p>Autor: ${libro.autor}</p>
           <hr />
         `;
-  
-      document.body.append(div);
-    });
-   });
+
+		librosContenedor.append(div);
+	});
+});
